ad118c27bbbbd222cea9b8ec20c4a19da11b781d
Justin Shaver <Justin Shaver>
2016-06-04 23:48:14 -0500

    Attempt at implementing google sign in

--- MyApplication/app/src/main/java/app/android/gimme/gimme/LoginActivity.java
+++ MyApplication/app/src/main/java/app/android/gimme/gimme/LoginActivity.java
@@ -3,6 +3,7 @@ package app.android.gimme.gimme;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -19,6 +20,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -28,6 +30,12 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -66,6 +74,19 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+
+        //mGoogleApiClient = new GoogleApiClient.Builder(this)
+          //      .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+            //    .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+              //  .build();
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
@@ -92,6 +113,54 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+    }
+/**
+    private void googleClicked(View view){
+         //Do google authentication ..
+        googleSignIn();
+        // Process user info
+        retrieveUserInfo();
+    }
+    private void googleSignIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);clear
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+            updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            updateUI(false);
+        }
+    }
+
+
+
+    private void facebookClicked(View view){
+        // Process user info
+        retrieveUserInfo();
+    }*/
+
+
+    private void registerClicked(View view){
+        Intent registerPageIntent = new Intent(getApplicationContext(),RegisterPageActivity.class);
+        startActivity(registerPageIntent);
     }
 
     private void populateAutoComplete() {
@@ -279,6 +348,15 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mEmailView.setAdapter(adapter);
     }
 
+    // Retrieve information for user before proceeding to home screen
+    public void retrieveUserInfo(){
+
+
+        // Move to home screen
+        Intent homeScreenIntent = new Intent(getApplicationContext(), HomeScreenActivity.class);
+        startActivity(homeScreenIntent);
+    }
+
 
     private interface ProfileQuery {
         String[] PROJECTION = {
@@ -333,6 +411,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
+                retrieveUserInfo();
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
---
+++ MyApplication/app/src/main/java/app/android/gimme/gimme/RegisterPageActivity.java
@@ -0,0 +1,13 @@
+package app.android.gimme.gimme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_page);
+    }
+}
